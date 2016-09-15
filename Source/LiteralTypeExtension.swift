56cf3b8c2c88f4adc5759b30ea4390c26e2e0459
Tom Baranes <tom.baranes@gmail.com>
2016-09-15 19:25:10 +0200

    Add LiteralTypeExtension

---
+++ Source/LiteralTypeExtension.swift
@@ -0,0 +1,27 @@
+//
+//  LiteralTypeExtension.swift
+//  SwiftyUtils
+//
+//  Created by Tom Baranes on 15/09/16.
+//  Copyright © 2016 Tom Baranes. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+public extension IntegerLiteralType {
+
+    // swiftlint:disable:next variable_name
+    public var f: CGFloat {
+        return CGFloat(self)
+    }
+
+}
+
+public extension FloatLiteralType {
+    // swiftlint:disable:next variable_name
+    public var f: CGFloat {
+        return CGFloat(self)
+    }
+
+}
