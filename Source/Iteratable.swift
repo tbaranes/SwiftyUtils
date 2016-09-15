dc2acfb976949e51ea164050ebf6ac4c8f258528
Tom Baranes <tom.baranes@gmail.com>
2016-09-15 23:05:36 +0200

    Introducing Iteratable

---
+++ Source/Iteratable.swift
@@ -0,0 +1,22 @@
+//
+//  Created by Tom Baranes on 15/09/16.
+//  Copyright © 2016 Tom Baranes. All rights reserved.
+//
+
+import Foundation
+
+public protocol Iteratable {}
+public extension Iteratable where Self: RawRepresentable & Hashable {}
+
+public func iterateEnum<T: Hashable>(_ from: T.Type) -> AnyIterator<T> {
+    var x = 0
+    return AnyIterator {
+        let next = withUnsafePointer(to: &x) {
+            $0.withMemoryRebound(to: T.self, capacity: 1) { $0.pointee }
+        }
+        defer {
+            x += 1
+        }
+        return next.hashValue == x ? next : nil
+    }
+}
