d7bea85d2a77e6dc1fb5930f2a131fa5976004d7
Tom Baranes <tom.baranes@gmail.com>
2017-02-15 14:20:57 +0100

    Add nearest{down,up} to IntExtension

--- SwiftyUtils/SwiftyUtils Core Tests/IntExtensionTests.swift
+++ SwiftyUtils/SwiftyUtils Core Tests/IntExtensionTests.swift
@@ -18,7 +18,11 @@ class IntExtensionTests: XCTestCase {
         super.tearDown()
     }
 
-    // MARK -
+}
+
+// MARK -
+
+extension IntExtensionTests {
 
     func testDigits() {
         XCTAssertEqual(33.digits, 2)
@@ -38,5 +42,58 @@ class IntExtensionTests: XCTestCase {
         XCTAssertTrue((-33).isNegative)
     }
 
+}
 
+// MARK - Round
+
+extension IntExtensionTests {
+
+    func testNearest() {
+        var value = 17578
+        XCTAssertEqual(value.nearestDozens, 17580)
+        XCTAssertEqual(value.nearestHundreds, 17600)
+        XCTAssertEqual(value.nearestThousands, 18000)
+
+        value = 17442
+        XCTAssertEqual(value.nearestDozens, 17440)
+        XCTAssertEqual(value.nearestHundreds, 17400)
+        XCTAssertEqual(value.nearestThousands, 17000)
+    }
+
+}
+
+// MARK - Round below
+
+extension IntExtensionTests {
+
+    func testNearestBelow() {
+        var value = 17578
+        XCTAssertEqual(value.nearestBelowDozens, 17570)
+        XCTAssertEqual(value.nearestBelowHundreds, 17500)
+        XCTAssertEqual(value.nearestBelowThousands, 17000)
+
+        value = 17442
+        XCTAssertEqual(value.nearestBelowDozens, 17440)
+        XCTAssertEqual(value.nearestBelowHundreds, 17400)
+        XCTAssertEqual(value.nearestBelowThousands, 17000)
+    }
+    
+}
+
+// MARK - Round Up
+
+extension IntExtensionTests {
+
+    func testNearestUp() {
+        var value = 17578
+        XCTAssertEqual(value.nearestUpDozens, 17580)
+        XCTAssertEqual(value.nearestUpHundreds, 17600)
+        XCTAssertEqual(value.nearestUpThousands, 18000)
+
+        value = 17442
+        XCTAssertEqual(value.nearestUpDozens, 17450)
+        XCTAssertEqual(value.nearestUpHundreds, 17500)
+        XCTAssertEqual(value.nearestUpThousands, 18000)
+    }
+    
 }
