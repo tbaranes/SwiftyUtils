f9052bc6af6b9507c906c9b4c9e382ac3da9a717
Tom Baranes <tom.baranes@gmail.com>
2017-04-09 13:45:29 +0200

    Add CGPoint operators extension

---
+++ SwiftyUtils/Tests/Extensions/CGPointExtensionTests.swift
@@ -0,0 +1,31 @@
+//
+//  CGPointExtensionTEsts.swift
+//  SwiftyUtils
+//
+//  Created by Tom Baranes on 09/04/2017.
+//  Copyright © 2017 Tom Baranes. All rights reserved.
+//
+
+import XCTest
+import SwiftyUtils
+
+class CGPointExtensionTests: XCTestCase {
+
+    func testOperators() {
+        var point = CGPoint(x: 10, y: 10)
+        let point2 = CGPoint(x: 10, y: 10)
+        XCTAssertEqual(CGPoint(x: 20, y: 20), point + point2)
+        XCTAssertEqual(CGPoint(x: 0, y: 0), point - point2)
+        XCTAssertEqual(CGPoint(x: 20, y: 20), point * 2)
+
+        point += point2
+        XCTAssertEqual(CGPoint(x: 20, y: 20), point)
+
+        point -= point2
+        XCTAssertEqual(CGPoint(x: 10, y: 10), point)
+
+        point *= 2
+        XCTAssertEqual(CGPoint(x: 20, y: 20), point)
+    }
+
+}
---
+++ SwiftyUtils/Tests/Extensions/CGSizeExtensionTests.swift
@@ -0,0 +1,9 @@
+//
+//  CGSizeExtensionTests.swift
+//  SwiftyUtils
+//
+//  Created by Tom Baranes on 09/04/2017.
+//  Copyright © 2017 Tom Baranes. All rights reserved.
+//
+
+import Foundation
