8314d145eeb028749eaba5a95c14c64e0fafdf34
Tom Baranes <tom.baranes@gmail.com>
2017-04-09 13:45:39 +0200

    Add CGSize operators extension

--- SwiftyUtils/Tests/Extensions/CGSizeExtensionTests.swift
+++ SwiftyUtils/Tests/Extensions/CGSizeExtensionTests.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2017 Tom Baranes. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import SwiftyUtils
+
+class CGSizeExtensionTests: XCTestCase {
+
+    func testOperators() {
+        var size = CGSize(width: 10, height: 10)
+        let size2 = CGSize(width: 10, height: 10)
+        XCTAssertEqual(CGSize(width: 20, height: 20), size + size2)
+        XCTAssertEqual(CGSize(width: 0, height: 0), size - size2)
+        XCTAssertEqual(CGSize(width: 20, height: 20), size * 2)
+
+        size += size2
+        XCTAssertEqual(CGSize(width: 20, height: 20), size)
+
+        size -= size2
+        XCTAssertEqual(CGSize(width: 10, height: 10), size)
+
+        size *= 2
+        XCTAssertEqual(CGSize(width: 20, height: 20), size)
+    }
+    
+}
