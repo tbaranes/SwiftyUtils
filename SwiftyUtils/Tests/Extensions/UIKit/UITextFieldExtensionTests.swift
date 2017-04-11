8b88e37f76be16f961275f2d4c4f4c4e7a98e8aa
Tom Baranes <tom.baranes@gmail.com>
2017-04-11 11:27:55 +0200

    Add tests to UITextField extension

---
+++ SwiftyUtils/Tests/Extensions/UIKit/UITextFieldExtensionTests.swift
@@ -0,0 +1,43 @@
+//
+//  UITextFieldExtensionTests.swift
+//  SwiftyUtils
+//
+//  Created by Tom Baranes on 11/04/2017.
+//  Copyright © 2017 Tom Baranes. All rights reserved.
+//
+
+import XCTest
+
+// MARK: Life cycle
+
+final class UITextFieldExtensionTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
+
+
+// MARK: - Placeholder
+
+extension UITextFieldExtensionTests {
+
+    func testSetPlaceHolderTextColor() {
+        let textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString(string: "Attributed Placeholder")
+        textField.setPlaceHolderTextColor(.blue)
+        let color = textField.attributedPlaceholder?.attribute(NSForegroundColorAttributeName, at: 0, effectiveRange: nil) as? UIColor
+        XCTAssertEqual(color, .blue)
+
+        textField.placeholder = nil
+        textField.setPlaceHolderTextColor(.yellow)
+        let emptyColor = textField.attributedPlaceholder?.attribute(NSForegroundColorAttributeName, at: 0, effectiveRange: nil) as? UIColor
+        XCTAssertNil(emptyColor)
+    }
+
+}
