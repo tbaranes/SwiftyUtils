fb85961dbfd41b95c3189d032fd28784fdaabf5f
Tom Baranes <tom.baranes@gmail.com>
2017-04-11 11:21:34 +0200

    Make UIStoryboard.main return an optional instead of fatalError

--- Sources/Extensions/UIKit/UIStoryboardExtension.swift
+++ Sources/Extensions/UIKit/UIStoryboardExtension.swift
@@ -7,10 +7,10 @@ import UIKit
 
 public extension UIStoryboard {
 
-    public static var main: UIStoryboard {
+    public class var main: UIStoryboard? {
         let bundle = Bundle.main
         guard let storyboardName = bundle.object(forInfoDictionaryKey: "UIMainStoryboardFile") as? String else {
-            fatalError("No main storyboard set in your app.")
+            return nil
         }
         return  UIStoryboard(name: storyboardName, bundle: bundle)
     }
