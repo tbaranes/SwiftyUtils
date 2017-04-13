3a7f9a2a189cdda9afb90be42c35906228cf0087
Tom Baranes <tom.baranes@gmail.com>
2017-04-13 11:20:30 +0200

    Updating app example

--- SwiftyUtils Example/iOS Example/Classes/ImagesViewController.swift
+++ SwiftyUtils Example/iOS Example/Classes/ImagesViewController.swift
@@ -23,12 +23,6 @@ final class ImagesViewController: UIViewController {
         }
     }
 
-    @IBOutlet weak var imageTinted: UIImageView! {
-        didSet {
-            imageTinted.image = imageTinted.image?.tinted(with: .green)
-        }
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
