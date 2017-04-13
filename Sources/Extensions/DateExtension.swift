fb670d6cdfd05b74f6bfe2647f745ef217d669d4
Tom Baranes <tom.baranes@gmail.com>
2017-04-13 12:20:50 +0200

    DateExtension cleanup

--- Sources/Extensions/DateExtension.swift
+++ Sources/Extensions/DateExtension.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-// MARK: - Format
+// MARK: - Initializers
 
 public extension Date {
 
@@ -18,14 +18,20 @@ public extension Date {
         self.init(timeInterval: 0, since: date)
     }
 
-    public func toString(dateStyle: DateFormatter.Style = .medium, timeStyle: DateFormatter.Style = .medium) -> String {
+}
+
+// MARK: - Format
+
+public extension Date {
+
+    public func string(dateStyle: DateFormatter.Style = .medium, timeStyle: DateFormatter.Style = .medium) -> String {
         let formatter = SUDateFormatter.shared
         formatter.dateStyle = dateStyle
         formatter.timeStyle = timeStyle
         return formatter.string(from: self as Date)
     }
 
-    public func toString(format: String) -> String {
+    public func string(format: String) -> String {
         let formatter = SUDateFormatter.shared
         formatter.dateFormat = format
         return formatter.string(from: self as Date)
@@ -33,32 +39,46 @@ public extension Date {
 
 }
 
-// MARK: - InBetweenDate
+// MARK: - Since
 
 public extension Date {
 
-    public func days(inBetween date: Date) -> Double {
+    public func days(since date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff / 86_400)
         return diff
     }
 
-    public func hours(inBetween date: Date) -> Double {
+    public func hours(since date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff / 3600)
         return diff
     }
 
-    public func minutes(inBetween date: Date) -> Double {
+    public func minutes(since date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff / 60)
         return diff
     }
 
-    public func seconds(inBetween date: Date) -> Double {
+    public func seconds(since date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff)
         return diff
+    }
+
+}
+
+// MARK: - IsIn
+
+public extension Date {
+
+    public var isInFuture: Bool {
+        return self > Date()
+    }
+
+    public var isInPast: Bool {
+        return self < Date()
     }
 
 }
