dae03d33282fa7721dd46657172eff2e9b56d9f0
Tom Baranes <tom.baranes@gmail.com>
2017-04-13 11:19:35 +0200

    Add tests for UIImage

---
+++ SwiftyUtils/Tests/Extensions/UIKit/UIImageExtensionTests.swift
@@ -0,0 +1,39 @@
+//
+//  UIImageExtensionTests.swift
+//  SwiftyUtils
+//
+//  Created by Tom Baranes on 13/04/2017.
+//  Copyright © 2017 Tom Baranes. All rights reserved.
+//
+
+import XCTest
+
+// MARK: Life cycle
+
+final class UIImageExtensionTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
+
+// MARK: - Colors
+
+extension UIImageExtensionTests {
+
+    func testRenderingModeOriginal() {
+        let image = UIImage(color: .green).original
+        XCTAssertEqual(image.original, image.withRenderingMode(.alwaysOriginal))
+    }
+
+    func testRenderingModeTemplate() {
+        let image = UIImage(color: .green).template
+        XCTAssertEqual(image.template, image.withRenderingMode(.alwaysTemplate))
+    }
+
+}
