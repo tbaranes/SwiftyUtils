922e41fdd759c781fa8cdc3d14ec665f635f7b5b
Tom Baranes <tom.baranes@icloud.com>
2019-11-12 15:31:52 +0100

    Fixed SPM support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
-let package = Package(
-  name: "SwiftyUtils"
-)
+let package = Package(name: "SwiftyUtils",
+                      platforms: [.macOS(.v10_10),
+                                  .iOS(.v8),
+                                  .tvOS(.v9),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "SwiftyUtils",
+                                          targets: ["SwiftyUtils"])],
+                      targets: [.target(name: "SwiftyUtils", path: "Sources")])
